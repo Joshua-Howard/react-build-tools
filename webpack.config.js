e6d7df69801ff394d0a9cef5edab30f2caf921e9
Josh <Joshua-Howard@users.noreply.github.com>
2020-01-21 17:52:01 -0500

    Add jsx and sass rules to webpack.config.js & webpack-dev-server --inline --hot start script

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,24 @@ const path = require('path');
 
 module.exports = {
   entry: './Frontend/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  },
   output: {
     path: path.join(__dirname, 'Frontend', 'public'),
     filename: 'bundle.js'
