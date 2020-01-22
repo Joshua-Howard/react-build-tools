3b76ea6f5d0233048b37cd6cad2c2a6609970688
Josh <Joshua-Howard@users.noreply.github.com>
2020-01-21 22:05:51 -0500

    Basic Filestructure and SCSS

---
+++ Frontend/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './index.scss';
+
+const App = () => {
+  return <div>Hey, I am React!</div>;
+};
+
+export default App;
--- Frontend/index.js
+++ Frontend/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './app';
 
-ReactDOM.render(
-  <div style={{ color: 'blue' }}>Hey, I am React!</div>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
