79abed38d5ca2cd7bec3a1ef0c017b116da4c434
Josh <Joshua-Howard@users.noreply.github.com>
2020-01-21 21:23:15 -0500

    npm i react react-dom

--- Frontend/index.js
+++ Frontend/index.js
@@ -1,1 +1,7 @@
-console.log('Hey JavaScript Here!');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <div style={{ color: 'blue' }}>Hey, I am React!</div>,
+  document.getElementById('root')
+);
