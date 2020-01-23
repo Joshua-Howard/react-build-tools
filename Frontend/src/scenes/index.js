2e8a9d65a9e80035136e537a08bcb0be48764719
Josh <Joshua-Howard@users.noreply.github.com>
2020-01-22 20:56:41 -0500

    Initial Grid Layout

--- Frontend/src/scenes/index.js
+++ Frontend/src/scenes/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../redux/actions/actions';
+import './styles.sass';
 
 const mapStateToProps = state => ({
   placeholder: state.interface.placeholder
@@ -15,7 +16,23 @@ const Scenes = props => {
   const { placeholder } = props;
   console.log(placeholder, 'This is the value of the placeholder state');
 
-  return <div>Hey, I am React!</div>;
+  return (
+    <div className="container-fluid d-flex justify-content-center align-items-center">
+      <div className="row containerRow">
+        <div className="col leftCol"> hey</div>
+
+        <div className="col rightCol">
+          <div className="row primaryRow" style={{ height: '70%' }}>
+            <div className="col" />
+          </div>
+
+          <div className="row secondaryRow">
+            <div className="col" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Scenes);
